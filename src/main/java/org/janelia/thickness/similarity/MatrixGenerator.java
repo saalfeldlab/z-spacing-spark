f7242e0bbfb4e439c41891da61085d03453c4ce7
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-07-28 15:30:44 -0400

    added missing interface MatrixGenerator

---
+++ src/main/java/org/janelia/thickness/similarity/MatrixGenerator.java
@@ -0,0 +1,26 @@
+package org.janelia.thickness.similarity;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import ij.process.FloatProcessor;
+import scala.Tuple2;
+
+public interface MatrixGenerator
+{
+
+	public static interface Factory
+	{
+		public MatrixGenerator create();
+	}
+
+	public JavaPairRDD< Tuple2< Integer, Integer >, FloatProcessor > generateMatrices(
+			final JavaSparkContext sc,
+			final JavaPairRDD< Tuple2< Integer, Integer >, Tuple2< FloatProcessor, FloatProcessor > > sectionPairs,
+			final int[] blockRadius,
+			final int[] stepSize,
+			final int range,
+			final int startIndex,
+			final int size );
+
+}
