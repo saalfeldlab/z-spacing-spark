20ee554ad873e2c1d00e913b833befd2f3e8216b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-07-28 15:49:04 -0400

    updated run scripts

--- src/main/bash/start-up/render-spark.sh
+++ src/main/bash/start-up/render-spark.sh
@@ -3,11 +3,10 @@
 ITERATION=$2
 ROOT_DIR=$(dirname `readlink -f $0`)
 BASE_DIR=`printf $ROOT_DIR/out/%02d $ITERATION`
-FLINTSTONE=$HOME/flintstone/src/main/shell
-INFLAME=$FLINTSTONE/inflame.sh
+FLINTSTONE=$HOME/flintstone/flintstone.sh
 N_NODES=$1
-JAR=${JAR:-$HOME/z_spacing-spark-scala-0.0.3-SNAPSHOT.jar}
+JAR=${JAR:-$HOME/z_spacing-spark-scala-1.0.0-SNAPSHOT.jar}
 CLASS=org.janelia.thickness.utility.SparkRender
 OUT=$BASE_DIR/render/%04d.tif
 CONFIG=$ROOT_DIR/config.json
-$INFLAME $N_NODES $JAR $CLASS $CONFIG $ITERATION $OUT
+$FLINTSTONE $N_NODES $JAR $CLASS "'$CONFIG'" $ITERATION $OUT
--- src/main/bash/start-up/run.sh
+++ src/main/bash/start-up/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 ROOT_DIR=$(dirname `readlink -f $0`)
-FLINTSTONE=$HOME/flintstone/src/main/shell
-INFLAME=$FLINTSTONE/inflame.sh
+FLINTSTONE=$HOME/flintstone/flintstone.sh
 N_NODES=$1
-JAR=${JAR:-$HOME/z_spacing-spark-scala-0.0.3-SNAPSHOT.jar}
+JAR=${JAR:-$HOME/z_spacing-spark-scala-1.0.0-SNAPSHOT.jar}
 CLASS=org.janelia.thickness.ZSpacing
 CONFIG=$ROOT_DIR/config.json
 SCALE=0
-$INFLAME $N_NODES $JAR $CLASS $CONFIG  >> runs.log
+$FLINTSTONE $N_NODES $JAR $CLASS "'$CONFIG'"  >> runs.log
 chmod -w $CONFIG
