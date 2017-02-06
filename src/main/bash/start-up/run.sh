df50ac26fe4885b3d34c0d6dbb166db82f4c6af0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-02-06 09:09:39 -0500

    Update run.sh

--- src/main/bash/start-up/run.sh
+++ src/main/bash/start-up/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 ROOT_DIR=$(dirname `readlink -f $0`)
-FLINTSTONE=$HOME/flintstone/src/main/shell
-INFLAME=$FLINTSTONE/inflame.sh
-N_NODES=$1
-JAR=${JAR:-$HOME/z_spacing-spark-0.0.3-SNAPSHOT.jar}
+FLINTSTONE=$HOME/flintstone/flintstone.sh
+MASTER_JOB_ID=$1
+JAR=${JAR:-$HOME/z_spacing-spark-0.0.5-SNAPSHOT.jar}
 CLASS=org.janelia.thickness.ZSpacing
 CONFIG=$ROOT_DIR/config.json
-SCALE=0
-$INFLAME $N_NODES $JAR $CLASS $CONFIG  >> runs.log
+SPARK_VERSION=2 $FLINTSTONE $N_NODES $JAR $CLASS $CONFIG  >> runs.log
 chmod -w $CONFIG
