032f057c3691af7a4cad596aa745fbcfea96ce29
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-02-06 09:18:25 -0500

    Fix bug in run.sh

--- src/main/bash/start-up/run.sh
+++ src/main/bash/start-up/run.sh
@@ -6,5 +6,5 @@ MASTER_JOB_ID=$1
 JAR=${JAR:-$HOME/z_spacing-spark-0.0.5-SNAPSHOT.jar}
 CLASS=org.janelia.thickness.ZSpacing
 CONFIG=$ROOT_DIR/config.json
-SPARK_VERSION=2 $FLINTSTONE $MASTER_JOB $JAR $CLASS $CONFIG  >> runs.log
+SPARK_VERSION=2 $FLINTSTONE $MASTER_JOB_ID $JAR $CLASS $CONFIG  >> runs.log
 chmod -w $CONFIG
